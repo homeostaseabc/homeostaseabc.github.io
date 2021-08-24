f9f8ebef4e23214c79703d72d03e44604e54525d
Douglas Kitagawa <86413676+homeostaseabc@users.noreply.github.com>
2021-08-24 15:21:49 -0300

    デジモン

--- js/packer.js
+++ js/packer.js
@@ -2,3 +2,4 @@ const encryptionKey = 'デジモンワールドは本物'
 const dWorld = 'Hekapad'
 
 const tamerTip = 'In a Binary world... Binary files are zeros and ones, and ASCII files are text!? Hekapad?'
+const textToBin = 'RapidTables'
